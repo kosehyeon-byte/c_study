60809c921e6dc94809c1a3d63d084bf81df0dcb0
kosehyeon-byte <rnoc987@naver.com>
2025-09-18 18:20:50 +0900

    250918 update

---
+++ Project1/prob13_1_4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int arr[6] = { 1,2,3,4,5,6 };
+	int* ptr1 = arr;
+	int* ptr2 = arr + 5;
+	int halfn = sizeof(arr) / sizeof(arr[0]) / 2;
+
+	for (int i = 0; i < halfn; i++) {
+		int temp = *(ptr1 + i);
+		*(ptr1 + i) = *(ptr2 - i);
+		*(ptr2 - i) = temp;
+	}
+
+	for (int i = 0; i < 2*halfn; i++) {
+		printf("%d ", *(ptr1 + i));
+	}
+	
+	return 0;
+}

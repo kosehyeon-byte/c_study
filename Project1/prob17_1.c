c722a3b8ed2eb91dafafb7d2820019e5e33a2fcf
kosehyeon-byte <rnoc987@naver.com>
2025-09-29 22:28:24 +0900

    250929

--- Project1/prob17_1.c
+++ Project1/prob17_1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #define _CRT_SECURE_NO_WARNINGS
 
+/*
 void MaxAndMin(int arr[5], int* max, int* min) {
 	for(int i = 0; i < 5; i++) {
 		printf("Number %d: ", i + 1);
@@ -27,3 +28,40 @@ int main(void)
 	printf("Min: %d\n", min);
 	return 0;
 }
+
+*/
+
+//------------------------------------------------------------
+
+/*
+void MaxAndMin(int arr[5], int** maxPtr, int** minPtr) {
+	for (int i = 0; i < 5; i++)
+	{
+		printf("Number %d: ", i + 1);
+		int yy = scanf("%d", &arr[i]);
+		if (i == 0) {
+			*maxPtr = &arr[i];
+			*minPtr = &arr[i];
+		}
+		else if (arr[i] > **maxPtr) {
+			*maxPtr = &arr[i];
+		}
+		else if (arr[i] < **minPtr) {
+			*minPtr = &arr[i];
+		}
+	}
+}
+
+
+int main(void)
+{
+	int * maxPtr;
+	int * minPtr;
+	int arr[5];
+	
+	MaxAndMin(arr, &maxPtr, &minPtr);
+	printf("Max: %d\n", *maxPtr);
+	printf("Min: %d\n", *minPtr);
+	return 0;
+}
+*/

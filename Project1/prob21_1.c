d1e6a705d92de7c84f8bf83383b3117b2e68db9c
kosehyeon-byte <rnoc987@naver.com>
2025-10-21 01:22:12 +0900

    251021

---
+++ Project1/prob21_1.c
@@ -0,0 +1,20 @@
+/*
+#include <stdio.h>
+
+int main(void) {
+	int ch;
+	ch = getchar();
+
+	if (97 <= ch && ch <= 122) {
+		putchar(ch - 32);
+	}
+	else if (65 <= ch && ch <= 90) {
+		putchar(ch + 32);
+	}
+	else putchar('?');
+
+	return 0;
+}
+*/
+
+

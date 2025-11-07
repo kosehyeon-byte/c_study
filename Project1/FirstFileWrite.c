c16d2d44b79215bac509941b05ce41a3fbe401b4
kosehyeon-byte <rnoc987@naver.com>
2025-11-07 19:15:27 +0900

    251107

---
+++ Project1/FirstFileWrite.c
@@ -0,0 +1,16 @@
+/*
+#include <stdio.h>
+
+int main(void) {
+	FILE* fp = fopen("data.txt", "wt");
+	if (fp == NULL) {
+		printf("File open error!\n");
+		return -1;
+	}
+
+	fputc('A', fp);
+	fclose(fp);
+	return 0;
+}
+*/
+
